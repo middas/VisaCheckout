51ff15906a729422d30f59db7156abad0bd4cc9b
jsommerc <jsommerc@visa.com>
2015-03-30 16:14:33 -0600

    added unit tests for on events

---
+++ VisaCheckout.Tests/OnOptionsTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VisaCheckout.VisaHelper.Options;
+
+namespace VisaCheckout.Tests
+{
+    [TestClass]
+    public class OnOptionsTest
+    {
+        [TestMethod]
+        public void GetHtmlTest()
+        {
+            IOptions options = new OnOptions();
+
+            string result = options.GetHtml();
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void GetHtmlWithOptionsTest()
+        {
+            IOptions options = new OnOptions
+            {
+                PaymentCancel = "cancel",
+                PaymentError = "error",
+                PaymentSuccess = "success"
+            };
+
+            string result = options.GetHtml();
+
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Contains("V.on(\"payment.success\", function(payment){success});"));
+            Assert.IsTrue(result.Contains("V.on(\"payment.error\", function(payment){error});"));
+            Assert.IsTrue(result.Contains("V.on(\"payment.cancel\", function(payment){cancel});"));
+        }
+    }
+}
