6aa3c1d379991b0b8414b95676a19f40f9c60fdc
middas <justin.middas@gmail.com>
2015-04-11 03:36:42 -0600

    added iadminrestrequest interface

---
+++ VisaCheckout/VisaHelper/REST/IAdminRestRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VisaCheckout.VisaHelper.REST
+{
+    public interface IAdminRestRequest : IVisaRestRequest
+    {
+        string Create();
+        string Update();
+        string Delete();
+    }
+}
