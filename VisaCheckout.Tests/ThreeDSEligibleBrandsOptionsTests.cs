face4d13ae7019242ae1ec6fcf1e85559722c04b
middas <justin.middas@gmail.com>
2015-04-11 18:45:08 -0600

    added threedseligiblebrandsoptions and unit test

---
+++ VisaCheckout.Tests/ThreeDSEligibleBrandsOptionsTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VisaCheckout.VisaHelper.Options;
+
+namespace VisaCheckout.Tests
+{
+    [TestClass]
+    public class ThreeDSEligibleBrandsOptionsTests
+    {
+        [TestMethod]
+        public void GetHtmlTest()
+        {
+            IOptions options = new ThreeDSEligibleBrandsOptions("abc123", SupportedCards.VISA | SupportedCards.AMEX);
+            string result = options.GetOptionString();
+
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Contains("\"threeDSEligibleBrands\":{"));
+            Assert.IsTrue(result.Contains("\"acquirerId\":\"abc123\""));
+            Assert.IsTrue(result.Contains("\"cardBrands\":[\"VISA\",\"AMEX\"]"));
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void GetHtmlNoAcquirerIDTest()
+        {
+            IOptions options = new ThreeDSEligibleBrandsOptions(null, SupportedCards.VISA | SupportedCards.AMEX);
+            string result = options.GetOptionString();
+
+            Assert.IsNull(result);
+        }
+    }
+}
---
+++ VisaCheckout/VisaHelper/Options/ThreeDSEligibleBrandsOptions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+using VisaCheckout.VisaHelper.Attributes;
+
+namespace VisaCheckout.VisaHelper.Options
+{
+    /// <summary>
+    /// Options for three DS eligible brands
+    /// </summary>
+    public class ThreeDSEligibleBrandsOptions : OptionsBase, IOptions
+    {
+        /// <summary>
+        /// The constructor
+        /// </summary>
+        /// <param name="acquirerID"></param>
+        /// <param name="cardBrands"></param>
+        public ThreeDSEligibleBrandsOptions(string acquirerID, SupportedCards cardBrands)
+        {
+            AcquirerID = acquirerID;
+            CardBrands = cardBrands;
+        }
+
+        /// <summary>
+        /// (Required) Acquirer ID associated with the processor.
+        /// </summary>
+        [Option("acquirerId")]
+        public string AcquirerID { get; set; }
+
+        /// <summary>
+        /// (Required) Card brands.
+        /// </summary>
+        [Option("cardBrands")]
+        public SupportedCards CardBrands { get; set; }
+
+        /// <summary>
+        /// Gets the options HTML
+        /// </summary>
+        /// <returns></returns>
+        public string GetOptionString()
+        {
+            if (string.IsNullOrEmpty(AcquirerID))
+            {
+                throw new ArgumentException("AcquirerID cannot be null");
+            }
+
+            StringBuilder sb = new StringBuilder("\"threeDSEligibleBrands\":{");
+            sb.Append(WriteOptionalJavascriptValue((ThreeDSEligibleBrandsOptions o) => o.AcquirerID));
+            sb.Append(WriteOptionalJavascriptValue((ThreeDSEligibleBrandsOptions o) => o.CardBrands));
+
+            sb.Length = sb.Length - 1;
+            sb.Append("}");
+
+            return sb.ToString();
+        }
+    }
+}
