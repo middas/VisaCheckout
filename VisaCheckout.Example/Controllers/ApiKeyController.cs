6d0dbd6d167b26c753a0fcf48fbbc1d8874b1986
jsommerc <jsommerc@visa.com>
2015-04-15 16:02:55 -0600

    added apikey example placeholders

---
+++ VisaCheckout.Example/Controllers/ApiKeyController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VisaCheckout.Example.Controllers
+{
+    public class ApiKeyController : Controller
+    {
+        //
+        // GET: /ApiKey/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Query()
+        {
+            return View();
+        }
+
+        public ActionResult Update()
+        {
+            return View();
+        }
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+	}
+}
